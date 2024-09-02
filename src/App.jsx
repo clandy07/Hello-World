651c16a10da377fdc514ac75810d359ca3fec95f
graysonLL <liam.m.a.jones@gmail.com>
2024-09-02 21:15:12 +0800

    From Liam branch, added Hi bro

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React, {useState} from 'react';
-import InputName from './components/InputName.jsx';
-import './App.css';
-
-
+import React, { useState } from "react";
+import InputName from "./components/InputName.jsx";
+import "./App.css";
 
 function App() {
-    const [name, setName] = useState('');
-    const [isSubmitted, setIsSubmitted] = useState(false);
+  const [name, setName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const handleNameSubmit = (userName) => {
-      setName(userName);
-      setIsSubmitted(true);
-    };
+  const handleNameSubmit = (userName) => {
+    setName(userName);
+    setIsSubmitted(true);
+  };
 
-    return (
-      <div className='body'>
-        {isSubmitted ? (
-          <h1>Hello, {name}!</h1>
-        ) : (
-          <InputName onSubmit={handleNameSubmit}/>
-        )}
-      </div>
-    )
+  return (
+    <div className="body">
+      {isSubmitted ? (
+        <h1>Hello, {name}! hi bro</h1>
+      ) : (
+        <InputName onSubmit={handleNameSubmit} />
+      )}
+    </div>
+  );
 }
 
 export default App;
-
